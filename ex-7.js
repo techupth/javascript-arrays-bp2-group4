817e9a33c543e48a22c07e90b2ca6824ae1688d7
Suwinai <suwinai.apionline@gmail.com>
2023-12-03 16:49:26 +0700

    feat: Array Assignment are finished

--- ex-7.js
+++ ex-7.js
@@ -46,3 +46,11 @@ let isArray;
 let creditCardTypeOfBlindermann;
 let productQuantityOfJoannet;
 let totalPurchaseOfDary;
+
+isArray = Array.isArray(orders);
+creditCardTypeOfBlindermann = orders[2].creditCardType;
+orders[2].creditCardType = "visa";
+productQuantityOfJoannet = orders[3].productQuantity;
+totalPurchaseOfDary = orders[1].productPrice * orders[1].productQuantity;
+delete orders[0];
+console.log(orders);
